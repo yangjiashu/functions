b6319c507295b7264469fbb7cd9c2193abc5cb99
yangjiashu <1098325805@qq.com>
2018-09-21 11:23:45 +0800

    模型实现成功，但未测试

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import numpy as np
-import Airline
-import Q_lam
+from Airline import *
+from Q_lam import *
 
 if __name__ == '__main__':
-    # block-1 initial
+    # initial
     capacity = 10
     times = np.array([5, 4, 3, 2, 1])
     states = []
     for c in range(capacity):
         for t in times:
-            states.append(tuple([c,t]))
+            states.append(tuple([c+1,t]))
 
     actions = [0.2, 0.4, 0.6, 0.8, 1.0]
     
@@ -17,17 +17,19 @@ if __name__ == '__main__':
     RL = Q_lam(states, actions)
     iter_times = 1000
 
-    # block-2 iteration
+    # block-1 iteration
     for i in range(iter_times + 1):
+        RL.e_table[:] = 0
         s = env.reset()
         a = np.random.choice(actions)
         
+        # block-2 episodes
         while True:
             s_, r, done, info = env.step(a)
             
             # not terminal
             if not done:
-                a_ = RL.choose_action(s_, 0.1)
+                a_, greedy = RL.choose_action(s_, 0.1)
                 RL.learn(s, a, r, s_, a_, done)
                 s = s_
                 a = a_
@@ -37,4 +39,4 @@ if __name__ == '__main__':
                 RL.learn(s, a, r, s_, a_,done) 
                 break
     if i % 50 == 0:
-        print(env.q_table)
+        print(RL.q_table)
